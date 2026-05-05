3a1568cbcb4fb772aecdd8bfbd81b5cdf015fa5b
justenoughwrong <justenoughwrong@gmail.com>
2026-05-04 22:07:16 -0400

    Program has basic functionality and UI.

---
+++ Scripts/app.py
@@ -0,0 +1,238 @@
+# Script for GUI class object
+
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import *
+from functools import partial
+from db_interact import DatabaseHandler
+
+
+########
+# MAIN #
+########
+
+
+class App(tk.Tk):
+    '''Main window'''
+
+    def __init__(self, title, size):
+        super().__init__()
+
+        # main setup
+        self.title(title)
+        self.geometry(f'{size[0]}x{size[1]}')
+        self.minsize(size[0], size[1])
+
+        self.database_handler = DatabaseHandler()
+
+        canvas_frame = ttk.Frame(self)
+        canvas_frame.pack(fill=BOTH, expand=True)
+
+        canvas = Canvas(canvas_frame)
+        canvas.pack(side=LEFT, fill=BOTH, expand=True)
+
+        scrollbar_ = ttk.Scrollbar(
+            canvas_frame, orient='vertical', command=canvas.yview)
+        scrollbar_.pack(side=RIGHT, fill=Y)
+
+        scroll_frame = ttk.Frame(canvas)
+        scroll_frame.bind('<Configure>', lambda event: canvas.configure(
+            scrollregion=canvas.bbox('all')))
+
+        canvas.configure(yscrollcommand=scrollbar_.set)
+        canvas.create_window((0, 0), window=scroll_frame, anchor='n')
+
+        WarframeFrame(scroll_frame, 0, self.database_handler).pack(
+            fill=X, expand=True, anchor='center')
+        WarframeFrame(scroll_frame, 1, self.database_handler).pack(
+            fill=X, expand=True, anchor='center')
+        WarframeFrame(scroll_frame, 2, self.database_handler).pack(
+            fill=X, expand=True)
+        WarframeFrame(scroll_frame, 3, self.database_handler).pack(
+            fill=X, expand=True)
+        WarframeFrame(scroll_frame, 4, self.database_handler).pack(
+            fill=X, expand=True)
+        WarframeFrame(scroll_frame, 5, self.database_handler).pack(
+            fill=X, expand=True)
+        WarframeFrame(scroll_frame, 6, self.database_handler).pack(
+            fill=X, expand=True)
+        WarframeFrame(scroll_frame, 7, self.database_handler).pack(
+            fill=X, expand=True)
+
+        self.bind('<KeyPress-Escape>', quit)
+
+        self.mainloop()
+
+
+#############
+# WARFRAMES #
+#############
+
+
+class WarframeFrame(ttk.Frame):
+    '''frame for warframe lookups'''
+
+    def __init__(self, parent, layout, database_handler):
+        super().__init__(parent)
+
+        print(self.winfo_parent)
+        self.database_handler = database_handler
+        self.visible = False
+        self.layout = layout
+
+        self.button = ttk.Button(self)
+        self.frame = ttk.Frame(self)
+        self.entry1_label = ttk.Label(self.frame)
+        self.entry1 = ttk.Entry(self.frame)
+        self.entry2_label = ttk.Label(self.frame)
+        self.entry2 = ttk.Entry(self.frame)
+        self.entry3_label = ttk.Label(self.frame)
+        self.entry3 = ttk.Entry(self.frame)
+        self.output = ttk.Label(self.frame, wraplength=200)
+
+        self.button.configure(command=partial(
+            self._toggle_frame, self.frame))
+        self.frame.configure(borderwidth=1, relief='sunken')
+
+        self.button.pack()
+
+        self._set_layout()
+
+    def _toggle_frame(self, frame):
+        if self.visible:
+            self.visible = False
+            frame.pack_forget()
+        else:
+            self.visible = True
+            frame.pack()
+
+    def _set_layout(self):
+        match self.layout:
+            case 0:
+                self._get_user_names_layout()
+            case 1:
+                self._get_warframe_names_layout()
+            case 2:
+                self._get_user_warframes_layout()
+            case 3:
+                self._get_warframe_users_layout()
+            case 4:
+                self._insert_user_layout()
+            case 5:
+                self._insert_warframe_layout()
+            case 6:
+                self._insert_user_warframe_layout()
+            case 7:
+                self._update_user_warframe_layout()
+            case _:
+                self.button.configure(text='This button is broken')
+
+    def _get_user_names_layout(self):
+        self.button.configure(text='Users')
+        self.output.pack()
+        self.output.bind('<Map>', self._get_user_names)
+
+    def _get_warframe_names_layout(self):
+        self.button.configure(text='Warframes')
+        self.output.pack()
+        self.output.bind('<Map>', self._get_warframe_names)
+
+    def _get_user_warframes_layout(self):
+        self.button.configure(text='Search for warframes')
+        self.entry1.pack()
+        self.output.pack()
+        self.entry1.bind('<Return>', self._get_user_warframes)
+
+    def _get_warframe_users_layout(self):
+        self.button.configure(text='Search for users')
+        self.entry1.pack()
+        self.output.pack()
+        self.entry1.bind('<Return>', self._get_warframe_users)
+
+    def _insert_user_layout(self):
+        self.button.configure(text='Add user')
+        self.entry1.pack()
+        self.output.pack()
+        self.entry1.bind('<Return>', self._insert_user)
+
+    def _insert_warframe_layout(self):
+        self.button.configure(text='Add warframe')
+        self.entry1.pack()
+        self.output.pack()
+        self.entry1.bind('<Return>', self._insert_warframe)
+
+    def _insert_user_warframe_layout(self):
+        self.button.configure(text='Add warframe to user')
+        self.entry1_label.configure(text='Enter user')
+        self.entry1_label.pack()
+        self.entry1.pack()
+        self.entry2_label.configure(text='Enter warframe')
+        self.entry2_label.pack()
+        self.entry2.pack()
+        self.output.pack()
+        self.entry1.bind('<Return>', self._insert_user_warframe)
+        self.entry2.bind('<Return>', self._insert_user_warframe)
+
+    def _update_user_warframe_layout(self):
+        self.button.configure(text="Alter user's warframe")
+        self.entry1_label.configure(text='Enter user')
+        self.entry1_label.pack()
+        self.entry1.pack()
+        self.entry2_label.configure(text='Enter warframe to alter')
+        self.entry2_label.pack()
+        self.entry2.pack()
+        self.entry3_label.configure(text='Enter new warframe')
+        self.entry3_label.pack()
+        self.entry3.pack()
+        self.output.pack()
+        self.entry1.bind('<Return>', self._update_user_warframe)
+        self.entry2.bind('<Return>', self._update_user_warframe)
+        self.entry3.bind('<Return>', self._update_user_warframe)
+
+    def _get_user_names(self, event):
+        self.output.configure(text=self.database_handler.get_user_names())
+
+    def _get_warframe_names(self, event):
+        self.output.configure(text=self.database_handler.get_warframe_names())
+
+    def _get_user_warframes(self, event):
+        self.output.configure(
+            text=self.database_handler.get_user_warframes_names(self.entry1.get()))
+
+    def _get_warframe_users(self, event):
+        self.output.configure(
+            text=self.database_handler.get_warframe_users_names(self.entry1.get()))
+
+    def _insert_user(self, event):
+        self.database_handler.insert_user(self.entry1.get())
+        self.output.configure(text=f'User "{self.entry1.get()}" added.')
+
+    def _insert_warframe(self, event):
+        self.database_handler.insert_warframe(self.entry1.get())
+        self.output.configure(text=f'Warframe "{self.entry1.get()}" added.')
+
+    def _insert_user_warframe(self, event):
+        user = self.entry1.get()
+        warframe = self.entry2.get()
+        if user == '' or warframe == '':
+            self.output.configure(text='One or more fields empty')
+        else:
+            self.database_handler.insert_user_warframe(
+                user, warframe)
+            self.output.configure(
+                text=f'"{warframe}" added to user {user}')
+
+    def _update_user_warframe(self, event):
+        user = self.entry1.get()
+        old_warframe = self.entry2.get()
+        new_warframe = self.entry3.get()
+        if user == '' or old_warframe == '' or new_warframe == '':
+            self.output.configure(text='One or more fields empty')
+        else:
+            self.database_handler.update_user_warframe(
+                user, old_warframe, new_warframe)
+            self.output.configure(
+                text=f'{user}\'s warframe "{old_warframe}" updated to "{new_warframe}"')
+
+
+App('Warframe Data App', (500, 500))
--- Scripts/db_interact.py
+++ Scripts/db_interact.py
@@ -4,169 +4,196 @@
 import sqlite3
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).parent.parent
 DATABASE_DIR = BASE_DIR / 'warframe.db'
 
-db_connect = sqlite3.connect(DATABASE_DIR)
-db_cur = db_connect.cursor()
 
+class DatabaseHandler():
+    '''database handler class'''
 
-def get_user_id(user_name) -> int:
-    '''return matched primary key'''
-    query = f'''
-        SELECT
-            u.user_id
-        FROM
-            users u
-        WHERE
-            u.user_name like "{user_name}"
-        '''
-    fetch = db_cur.execute(query)
-    result = int(fetch.fetchone()[0])
-    return result
+    def __init__(self):
+        self._db_connect = sqlite3.connect(DATABASE_DIR)
+        self._db_cur = self._db_connect.cursor()
 
+    def __del__(self):
+        self._db_cur.close()
+        self._db_connect.close()
+        print('database connection closed')
 
-def get_warframe_id(warframe_name) -> int:
-    '''return matched primary key'''
-    query = f'''
-        SELECT
-            w.warframe_id
-        FROM
-            warframes w
-        WHERE
-            w.warframe_name like "{warframe_name}"
-        '''
-    fetch = db_cur.execute(query)
-    result = int(fetch.fetchone()[0])
-    return result
+    def _get_user_id(self, user_name) -> int:
+        '''return primary key'''
+        query = f'''
+            SELECT
+                u.user_id
+            FROM
+                users u
+            WHERE
+                u.user_name like "{user_name}"
+            '''
+        fetch = self._db_cur.execute(query)
+        result = int(fetch.fetchone()[0])
+        return result
 
+    def _get_warframe_id(self, warframe_name) -> int:
+        '''return primary key'''
+        query = f'''
+            SELECT
+                w.warframe_id
+            FROM
+                warframes w
+            WHERE
+                w.warframe_name like "{warframe_name}"
+            '''
+        fetch = self._db_cur.execute(query)
+        result = int(fetch.fetchone()[0])
+        return result
 
-def get_user_warframes(user_name) -> tuple:
-    '''return list of matched primary keys'''
-    query = f'''
-        SELECT
-            w.warframe_id
-        FROM
-            warframes w
-            JOIN warframes_users_lookup wul ON w.warframe_id = wul.warframe_id
-            JOIN users u ON u.user_id = wul.user_id
-        WHERE
-            user_name like "{user_name}"
-        '''
-    fetch = db_cur.execute(query)
-    result_list = [int(row[0]) for row in fetch.fetchall()]
-    result = tuple(result_list)
-    return result
+    def _get_user_warframes_ids(self, user_name) -> tuple:
+        '''return tuple of primary keys'''
+        query = f'''
+            SELECT
+                w.warframe_id
+            FROM
+                warframes w
+                JOIN warframes_users_lookup wul ON w.warframe_id = wul.warframe_id
+                JOIN users u ON u.user_id = wul.user_id
+            WHERE
+                user_name like "{user_name}"
+            '''
+        fetch = self._db_cur.execute(query)
+        result_list = [int(row[0]) for row in fetch.fetchall()]
+        result = tuple(result_list)
+        return result
 
+    def _get_warframe_users_ids(self, warframe_name) -> tuple:
+        '''return tuple of primary keys'''
+        query = f'''
+            SELECT
+                u.user_id
+            FROM
+                users u
+                JOIN warframes_users_lookup wul ON u.user_id
+                JOIN warframes w ON w.warframe_id = wul.warframe_id
+            WHERE
+                w.warframe_name like "{warframe_name}"
+            '''
+        fetch = self._db_cur.execute(query)
+        result_list = [int(row[0]) for row in fetch.fetchall()]
+        result = tuple(result_list)
+        return result
 
-def get_warframe_users(warframe_name) -> tuple:
-    '''return list of matched primary keys'''
-    query = f'''
-        SELECT
-            u.user_id
-        FROM
-            users u
-            JOIN warframes_users_lookup wul ON u.user_id
-            JOIN warframes w ON w.warframe_id = wul.warframe_id
-        WHERE
-            w.warframe_name like "{warframe_name}"
-        '''
-    fetch = db_cur.execute(query)
-    result_list = [int(row[0]) for row in fetch.fetchall()]
-    result = tuple(result_list)
-    return result
+    def get_user_warframes_names(self, user_name) -> tuple:
+        '''return tuple of data'''
+        query = f'''
+            SELECT
+                w.warframe_name
+            FROM
+                warframes w
+                JOIN warframes_users_lookup wul ON w.warframe_id = wul.warframe_id
+                JOIN users u ON u.user_id = wul.user_id
+            WHERE
+                user_name like "{user_name}"
+            '''
+        fetch = self._db_cur.execute(query)
+        result_list = [str(row[0]) for row in fetch.fetchall()]
+        result = tuple(result_list)
+        return result
 
+    def get_warframe_users_names(self, warframe_name) -> tuple:
+        '''return tuple of data'''
+        query = f'''
+            SELECT
+                u.user_name
+            FROM
+                users u
+                JOIN warframes_users_lookup wul ON u.user_id
+                JOIN warframes w ON w.warframe_id = wul.warframe_id
+            WHERE
+                w.warframe_name like "{warframe_name}"
+            '''
+        fetch = self._db_cur.execute(query)
+        result_list = [str(row[0]) for row in fetch.fetchall()]
+        result = tuple(result_list)
+        return result
 
-def get_users_names(user_id_list) -> tuple:
-    '''return list matching provided primary keys'''
-    user_id_tuple = tuple(user_id_list)
-    query = f'''
-        SELECT
-            u.user_name
-        FROM
-            users u
-        WHERE
-            u.user_id IN {user_id_tuple}
-        '''
-    fetch = db_cur.execute(query)
-    result_list = [str(row[0]) for row in fetch.fetchall()]
-    result = tuple(result_list)
-    return result
+    def get_user_names(self) -> tuple:
+        '''return tuple of data'''
+        query = '''
+            SELECT
+                u.user_name
+            FROM
+                users u
+            '''
+        fetch = self._db_cur.execute(query)
+        result_list = [str(row[0]) for row in fetch.fetchall()]
+        result = tuple(result_list)
+        return result
 
+    def get_warframe_names(self) -> tuple:
+        '''return tuple of data'''
+        query = '''
+            SELECT
+                w.warframe_name
+            FROM
+                warframes w
+            '''
+        fetch = self._db_cur.execute(query)
+        result_list = [str(row[0]) for row in fetch.fetchall()]
+        result = tuple(result_list)
+        return result
 
-def get_warframes_names(warframe_id_list) -> list:
-    '''return list matching provided primary keys'''
-    warframe_id_tuple = tuple(warframe_id_list)
-    query = f'''
-        SELECT
-            w.warframe_name
-        FROM
-            warframes w
-        WHERE
-            w.warframe_id IN {warframe_id_tuple}
-        '''
-    fetch = db_cur.execute(query)
-    result_list = [str(row[0]) for row in fetch.fetchall()]
-    result = tuple(result_list)
-    return result
+    def insert_user(self, user_name) -> None:
+        '''insert row'''
+        query = f'''
+            INSERT INTO
+                users (user_name)
+            VALUES
+                ("{user_name}")
+            '''
+        self._db_cur.execute(query)
+        self._db_connect.commit()
 
+    def insert_warframe(self, warframe_name) -> None:
+        '''insert row'''
+        query = f'''
+            INSERT INTO
+                warframes (warframe_name)
+            VALUES
+                ("{warframe_name}")
+            '''
+        self._db_cur.execute(query)
+        self._db_connect.commit()
 
-def insert_user(user_name) -> None:
-    '''insert row'''
-    query = f'''
-        INSERT INTO
-            users (user_name)
-        VALUES
-            ("{user_name}")
-        '''
-    db_cur.execute(query)
-    db_connect.commit()
+    def insert_user_warframe(self, user_name, warframe_name) -> None:
+        '''insert primary key pair'''
+        user_id = self._get_user_id(user_name)
+        warframe_id = self._get_warframe_id(warframe_name)
+        query = f'''
+            INSERT INTO
+                warframes_users_lookup (user_id, warframe_id)
+            VALUES
+                ({user_id}, {warframe_id})
+            '''
+        self._db_cur.execute(query)
+        self._db_connect.commit()
 
-
-def insert_warframe(warframe_name) -> None:
-    '''insert row'''
-    query = f'''
-        INSERT INTO
-            warframes (warframe_name)
-        VALUES
-            ("{warframe_name}")
-        '''
-    db_cur.execute(query)
-    db_connect.commit()
-
-
-def insert_user_warframe(user_name, warframe_name) -> None:
-    '''insert primary key pair'''
-    user_id = get_user_id(user_name)
-    warframe_id = get_warframe_id(warframe_name)
-    query = f'''
-        INSERT INTO
-            warframes_users_lookup (user_id, warframe_id)
-        VALUES
-            ({user_id}, {warframe_id})
-        '''
-    db_cur.execute(query)
-    db_connect.commit()
-
-
-def update_user_warframe(user_name, old_warframe_name, new_warframe_name) -> None:
-    '''update primary key pair'''
-    query = f'''
-        UPDATE
-            warframes_users_lookup
-        SET (user_id, warframe_id) = (
-            (SELECT u.user_id FROM users u WHERE u.user_name LIKE "{user_name}"),
-            (SELECT w.warframe_id FROM warframes w WHERE w.warframe_name LIKE "{new_warframe_name}")
-        )
-        WHERE
-            warframes_users_lookup.user_id = (
-                SELECT u.user_id FROM users u WHERE u.user_name LIKE "{user_name}"
+    def update_user_warframe(self, user_name, old_warframe_name, new_warframe_name) -> None:
+        '''update primary key pair'''
+        query = f'''
+            UPDATE
+                warframes_users_lookup
+            SET (user_id, warframe_id) = (
+                (SELECT u.user_id FROM users u WHERE u.user_name LIKE "{user_name}"),
+                (SELECT w.warframe_id FROM warframes w WHERE w.warframe_name LIKE "{new_warframe_name}")
             )
-        AND
-            warframes_users_lookup.warframe_id = (
-                SELECT w.warframe_id FROM warframes w WHERE w.warframe_name LIKE "{old_warframe_name}"
-            )
-        '''
-    db_cur.execute(query)
-    db_connect.commit()
+            WHERE
+                warframes_users_lookup.user_id = (
+                    SELECT u.user_id FROM users u WHERE u.user_name LIKE "{user_name}"
+                )
+            AND
+                warframes_users_lookup.warframe_id = (
+                    SELECT w.warframe_id FROM warframes w WHERE w.warframe_name LIKE "{old_warframe_name}"
+                )
+            '''
+        self._db_cur.execute(query)
+        self._db_connect.commit()
